5b716cfe59b720e5c54471148275894e323bd688
Andrey9417 <andreypovlat@gmail.com>
2021-05-26 23:11:41 +0300

    added 1st test for Rozetka

---
+++ src/test/java/TestGoogleSearch.java
@@ -0,0 +1,58 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class TestGoogleSearch {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    String initialUrl = "https://www.google.com/";
+    String searchText = "iphone kyiv buy";
+    //String partialLink = "stylus.ua";
+    boolean elementFound = false;
+    List<WebElement> listOfElements;
+
+
+    @BeforeClass
+    public void setupBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+        driver.close();
+    }
+
+    @BeforeMethod
+    public void navigateToSite(){
+        driver.get(initialUrl);
+    }
+
+    @Test
+    public void test(){
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys(searchText + Keys.ENTER);
+
+        for(int i = 0; i<5; i++){
+            listOfElements = driver.findElements(By.xpath("//cite[contains(text(), 'stylus.ua')]"));
+            if(!listOfElements.isEmpty()) {
+                System.out.println("STYLUS.UA found on "+(i+1)+" page");
+                elementFound = true;
+                break;
+            }
+            driver.findElement(By.id("pnnext")).click();
+        }
+        assertTrue(elementFound, "STYLUS.UA not found on first 5 pages");
+        // //cite[contains(text(), 'stylus.ua')]
+    }
+}
---
+++ src/test/java/TestGuruLogin.java
@@ -0,0 +1,103 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.testng.Assert.assertEquals;
+
+public class TestGuruLogin {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    String initialUrl = "http://demo.guru99.com/Agile_Project/Agi_V1/index.php";
+    String userName = "1303";
+    String userPassword = "Guru99";
+    String incorrectCredsMessage = "User or Password is not valid";
+
+    @BeforeClass
+    public void setupBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+        driver.close();
+    }
+
+    @BeforeMethod
+    public void navigateToSite(){
+        driver.get(initialUrl);
+    }
+
+    @Test
+    public void positiveTest_LOGIN(){
+        driver.findElement(By.name("uid")).sendKeys(userName);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(userPassword+ Keys.ENTER);
+        //driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+
+        WebElement logoutLink = wait.until(presenceOfElementLocated(By.linkText("Log out")));
+        logoutLink.click();
+
+        driver.switchTo().alert().accept();
+
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals(currentUrl, initialUrl);
+    }
+
+    @Test
+    public void negativeTest1_LOGIN(){
+        driver.findElement(By.name("uid")).sendKeys(userName + Keys.ENTER);
+
+        assertEquals(driver.switchTo().alert().getText(), incorrectCredsMessage);
+        driver.switchTo().alert().accept();
+
+        assertEquals(driver.getCurrentUrl(), initialUrl);
+    }
+
+    @Test
+    public void negativeTest2_LOGIN(){
+        driver.findElement(By.name("password")).sendKeys(userPassword + Keys.ENTER);
+
+        assertEquals(driver.switchTo().alert().getText(), incorrectCredsMessage);
+        driver.switchTo().alert().accept();
+
+        assertEquals(driver.getCurrentUrl(), initialUrl);
+    }
+
+    @Test
+    public void negativeTest3_LOGIN(){
+        driver.findElement(By.name("uid")).sendKeys(userName + userName);
+        driver.findElement(By.name("password")).sendKeys(userPassword);
+        driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+
+        assertEquals(driver.switchTo().alert().getText(), incorrectCredsMessage);
+        driver.switchTo().alert().accept();
+
+        assertEquals(driver.getCurrentUrl(), initialUrl);
+    }
+
+    @Test
+    public void negativeTest4_LOGIN(){
+        driver.findElement(By.name("uid")).sendKeys(userName);
+        driver.findElement(By.name("password")).sendKeys(userPassword+userPassword);
+        driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+
+        assertEquals(driver.switchTo().alert().getText(), incorrectCredsMessage);
+        driver.switchTo().alert().accept();
+
+        assertEquals(driver.getCurrentUrl(), initialUrl);
+    }
+
+
+}
